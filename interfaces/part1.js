2465794ea3113a0d654aec94b4d8269a896310eb
unknown <ruben.lousada.dev@gmail.com>
2024-12-03 02:22:30 +0000

    register and login

--- interfaces/part1.js
+++ interfaces/part1.js
@@ -1,46 +1,40 @@
-async function getData() {
-    const url = "http://localhost:8090/text";
-    try {
-        const response = await fetch(url);
-        console.log(response)
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
 
-        const json = await response.json();
-        let element = document.getElementById("tbody");
+async function login(event) {
+    event.preventDefault()
+    const email = document.getElementById('login_email').value;
+    const password = document.getElementById('login_password').value;
+    const good = await fetch('http://localhost:8090/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: email, password: password })
+    })
 
-        for (const value of json) {
-            element.innerHTML += "<tr><td>" + value.text + " </td></tr>";
-        }
-    } catch (error) {
-        console.error(error.message);
+    if (good.ok) {
+        alert("Login successfully")
+    } else {
+        alert("Error logging in")
     }
 }
 
-getData()
-
-async function handleGoodSubmit(event) {
+async function register(event) {
     event.preventDefault()
-    const textValue = document.getElementById('c_text').value;
-    await fetch('http://localhost:8090/text/safe', {
+    const email = document.getElementById('register_email').value;
+    const password = document.getElementById('register_password').value;
+
+    const bad = await fetch('http://localhost:8090/accounts', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ text: textValue })
+        body: JSON.stringify({ email: email, password: password })
     })
-}
 
-async function handleBadSubmit(event) {
-    event.preventDefault()
-    const textValue = document.getElementById('b_text').value;
-    const bad = await fetch('http://localhost:8090/text', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: textValue })
-    })
+    if (bad.ok) {
+        alert("Created successfully")
+    } else {
+        alert("Error creating account")
+    }
     console.log(bad)
 }
